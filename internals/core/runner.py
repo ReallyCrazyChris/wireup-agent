434e96e7ef103da4427e90546c01816360d45df9
Chris Bentley <chris.bentley@wireup.io>
2018-11-20 17:50:20 +0100

    added excpetion handlers when either wifi or wensocket is not available

--- internals/core/runner.py
+++ internals/core/runner.py
@@ -1,11 +1,12 @@
 try:
-    import asyncio            
+    import asyncio
+    from wstransport import getwebsocket            
 except ImportError:
     import uasyncio as asyncio   
     from wifi import joinwifi    
   
 from udptransport import getsocket, receiveudp, sendudp
-from wstransport import getwebsocket
+
 from reactor import react
 from config import ip
 
@@ -16,21 +17,27 @@ async def mainTask(sock):
         sendudp(sock)
         await asyncio.sleep(0)
 
-async def websockTask():
-    websock = getwebsocket()
+async def websockTask(websock):
     while 1:
         websock.serveonce()
         await asyncio.sleep(0)
 
 def listen():
 
-    try:#micropython approach
+    loop = asyncio.get_event_loop()
+
+    try:# to join a wfinetwork (micropython)
         sock = getsocket(joinwifi())
+        loop.create_task( mainTask(sock) )  
     except Exception:
         #cpython approach
         sock = getsocket(ip)
+        loop.create_task( mainTask(sock) )  
 
-    loop = asyncio.get_event_loop()
-    loop.create_task( mainTask(sock) )
-    loop.create_task( websockTask() )
+    try:# to create a websocket
+        websock = getwebsocket()
+        loop.create_task( websockTask(websock) )
+    except Exception:
+        pass
+
     loop.run_forever()
